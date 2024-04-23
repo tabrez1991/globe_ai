5966165f8fae375dab903db7669f7b8b7f46b345
Md Tarvez Ansari <mdtarvezansari@Mds-MacBook-Air.local>
2024-04-24 00:21:46 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
+import withPWA from 'next-pwa';
+
 const nextConfig = {
-  reactStrictMode: true, 
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
-  output: "export"
-};
-
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
   reactStrictMode: true, 
   swcMinify: true,
   compiler: {
@@ -22,8 +12,8 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/]
   }
-});
+};
+
+module.exports = withPWA(nextConfig);
 // module.exports = nextConfig;
