ab4839237a7244f492720ee92489bc26d48f73d9
Md Tarvez Ansari <mdtarvezansari@Mds-MacBook-Air.local>
2024-04-28 00:03:44 +0530

    update

---
+++ components/WeatherWidget/index.tsx
@@ -0,0 +1,58 @@
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+
+const WeatherWidget = () => {
+  const [locationInfo, setLocationInfo] = React.useState<any>(null);
+
+  React.useEffect(() => {
+    // Check if geolocation is supported by the browser
+    if ("geolocation" in navigator) {
+      // Get user's current position
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        // Call API to get city/state information based on latitude and longitude
+        fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+          .then(response => response.json())
+          .then(data => {
+            // Update state with city/state information
+            setLocationInfo({
+              city: data.city,
+              state: data.principalSubdivision
+            });
+          })
+          .catch(error => {
+            console.error('Error fetching location:', error);
+          });
+      }, (error) => {
+        console.error('Error getting geolocation:', error);
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
+  return (
+    <Container sx={{ position: "relative" }}>
+      <Box sx={{ width: "250px", border: "2px solid #000", background: "#000000b3", p: 2, borderRadius: "4px", position: "absolute", top: "150px", right: "2%" }}>
+        <Box sx={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+          <Typography sx={{ color: "#ffa40e", mr: 2, fontSize: "1.5rem" }}><i className="ri-map-pin-fill"></i></Typography>
+          <Typography sx={{ fontSize: "1.5rem" }}>{locationInfo?.city}</Typography>
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", mt: 1 }}>
+          <Box sx={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+            <Typography sx={{ color: "#fff", mr: 1, fontSize: "0.75rem" }}><i className="ri-circle-fill"></i></Typography>
+            <Typography sx={{ fontSize: "0.75rem" }}>INDEX: 123</Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+            <Typography sx={{ color: "#fff", mr: 1, fontSize: "0.75rem" }}><i className="ri-circle-fill"></i></Typography>
+            <Typography sx={{ fontSize: "0.75rem" }}>INDEX: 123</Typography>
+          </Box>
+        </Box>
+      </Box >
+    </Container>
+  )
+}
+
+export default WeatherWidget
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import styles from './page.module.css'
 import Header from '../../components/Header'
 import React from 'react';
 import SideNav from '../../components/SideNav';
+import WeatherWidget from '../../components/WeatherWidget';
 
 export default function Home() {
   const [openNav, setOpenNav] = React.useState(false);
@@ -13,70 +14,15 @@ export default function Home() {
     <Box className={styles.main} id='background-element'>
       {/* Header */}
       <Header handleNav={() => setOpenNav(!openNav)} />
+
+      {/* SideNav */}
       {openNav && <SideNav open={openNav} handleNav={() => setOpenNav(!openNav)} />}
 
-
-      {/* <div className="offcanvas offcanvas-end" data-bs-scroll="true" tabIndex={-1} id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
-        <div className="offcanvas-header">
-          <button type="button" className="btn-close ms-auto" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-
-        <div className="offcanvas-body d-flex flex-column justify-content-center align-items-center">
-          <nav>
-            <ul>
-              <li>
-                <a href="login.html">Login Form</a>
-              </li>
-
-              <li>
-                <a href="register.html">Create an account</a>
-              </li>
-
-              <li>
-                <a href="password-reset.html">Password Reset</a>
-              </li>
-
-              <li>
-                <a href="404.html">404 Page</a>
-              </li>
-
-              <li>
-                <a href="contact.html">Contact Form</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-
-
-      <div className="modal fade" id="subscribeModal" tabIndex={-1} aria-labelledby="subscribeModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-          <div className="modal-content">
-            <div className="modal-header">
-
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div className="modal-body">
-              <form action="#" method="get" className="custom-form mt-lg-4 mt-2" role="form">
-                <h2 className="modal-title" id="subscribeModalLabel">Stay up to date</h2>
-
-                <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" className="form-control" placeholder="your@email.com" required />
-
-                <button type="submit" className="form-control">Notify</button>
-              </form>
-            </div>
-
-            <div className="modal-footer justify-content-center">
-              <p>By signing up, you agree to our Privacy Notice</p>
-            </div>
-          </div>
-        </div>
-      </div>*/}
+      {/* Weather Widget */}
+      <WeatherWidget />
 
 
       <section className={styles["hero-section"]} style={{ display: "flex", justifyContent: "center", alignItems: "center" }} id="section_1">
-
         <div className={styles["video-wrap"]}>
           <video autoPlay loop muted className={styles["custom-video"]} poster="">
             <source src="./videos/video.mp4" type="video/mp4" />
